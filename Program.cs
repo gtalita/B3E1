f975feaf76b4bc5ca1575522db9247c5a85d89c8
gtalita <167032543+gtalita@users.noreply.github.com>
2024-09-26 23:09:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace B3E1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Curso curso = new Curso("Curso Técnico de Informática Integrado ao Ensino Médio", "CTII", Periodicidade.Anual, 4);
+
+
+            curso.lperiodos[0].ldisciplinas.Add(new Disciplina("Matemática", "CBTMA1", AreaConhecimento.Matematica, NucleoEstruturante.Comum, 114, 3, 1));
+            
+
+            foreach (Curso.Periodo item in CTII.periodos)
+            { 
+                switch(periodo1)
+                    case 1:
+                    CTII.periodos.Contains(item);
+                    break;
+            }
+
+        }
+    }
+}
