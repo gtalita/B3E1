70a7daff2108e2c3146b504f09bfa046a6eeea26
gtalita <167032543+gtalita@users.noreply.github.com>
2024-09-26 23:09:29 -0300

    Add files via upload

---
+++ enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3E1
+{
+    internal enum Periodicidade
+    {
+        Semestral, 
+        Anual
+    }
+
+    internal enum AreaConhecimento
+    {
+        Linguagens, Matematica, CienciasNatureza,
+        CienciasHumanas, Tecnica
+    }
+
+    internal enum NucleoEstruturante 
+    { 
+        Comum, 
+        Tecnologico 
+    }
+}
